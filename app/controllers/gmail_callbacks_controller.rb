0f55d7c08085d27023ab2ae64bcccb45e103dd90
Kakiuchi Shusuke <kakiuchis@gmail.com>
2018-05-03 15:37:56 +0900

    callback prduction development if

--- app/controllers/gmail_callbacks_controller.rb
+++ app/controllers/gmail_callbacks_controller.rb
@@ -1,25 +1,44 @@
 class GmailCallbacksController < ApplicationController
   before_action :authenticate_user!
   def redirect
-    client = Signet::OAuth2::Client.new({
-      client_id: ENV.fetch('GOOGLE_API_CLIENT_ID_DEVELOPMENT'),
-      client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET_DEVELOPMENT'),
-      authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
-      scope: Google::Apis::GmailV1::AUTH_GMAIL_READONLY,
-      redirect_uri: url_for(action: :callback)
-	})
+    if Rails.env.production?
+      client = Signet::OAuth2::Client.new({
+        client_id: ENV.fetch('GOOGLE_API_CLIENT_ID_PRODUCTION'),
+        client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET_PRODUCTION'),
+        authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
+        scope: Google::Apis::GmailV1::AUTH_GMAIL_READONLY,
+        redirect_uri: url_for(action: :callback),
+  	  })
+    else
+      client = Signet::OAuth2::Client.new({
+        client_id: ENV.fetch('GOOGLE_API_CLIENT_ID_DEVELOPMENT'),
+        client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET_DEVELOPMENT'),
+        authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
+        scope: Google::Apis::GmailV1::AUTH_GMAIL_READONLY,
+        redirect_uri: url_for(action: :callback),
+      })
+    end
 	redirect_to client.authorization_uri.to_s
   end
 
   def callback
-    client = Signet::OAuth2::Client.new({
-      client_id: ENV.fetch('GOOGLE_API_CLIENT_ID_DEVELOPMENT'),
-      client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET_DEVELOPMENT'),
-      token_credential_uri: 'https://www.googleapis.com/oauth2/v3/token',
-      redirect_uri: url_for(action: :callback),
-      code: params[:code]
-    })
-
+    if Rails.env.production?
+      client = Signet::OAuth2::Client.new({
+        client_id: ENV.fetch('GOOGLE_API_CLIENT_ID_PRODUCTION'),
+        client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET_PRODUCTION'),
+        token_credential_uri: 'https://www.googleapis.com/oauth2/v3/token',
+        redirect_uri: url_for(action: :callback),
+        code: params[:code]
+      })
+    else
+      client = Signet::OAuth2::Client.new({
+        client_id: ENV.fetch('GOOGLE_API_CLIENT_ID_DEVELOPMENT'),
+        client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET_DEVELOPMENT'),
+        token_credential_uri: 'https://www.googleapis.com/oauth2/v3/token',
+        redirect_uri: url_for(action: :callback),
+        code: params[:code]
+      })
+    end
     response = client.fetch_access_token!
     session[:access_token] = response['access_token']
     redirect_to url_for(action: :messages)
@@ -27,14 +46,14 @@ class GmailCallbacksController < ApplicationController
 
   def messages
     uri = URI.parse("https://www.googleapis.com/gmail/v1/users/me/messages/")
-	request = Net::HTTP::Get.new(uri)
-	request["Authorization"] = "Bearer #{session[:access_token]}"
-	req_options = {
-	  use_ssl: uri.scheme == "https",
-	}
-	response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-	  http.request(request)
-	end
-	@responses = JSON.parse(response.body)["messages"]
+  	request = Net::HTTP::Get.new(uri)
+  	request["Authorization"] = "Bearer #{session[:access_token]}"
+  	req_options = {
+  	  use_ssl: uri.scheme == "https",
+  	}
+  	response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+  	  http.request(request)
+  	end
+  	@responses = JSON.parse(response.body)["messages"]
   end
 end
