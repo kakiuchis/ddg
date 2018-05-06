2b7b72213e3c040c3aa2a1ffbe6b75a22e59f58c
Kakiuchi Shusuke <kakiuchis@gmail.com>
2018-05-06 17:55:18 +0900

    change top to dashbord

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  
 
   resources :messages
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
-  root 'top#index'
+
+  root 'dashboad#index'
+
   get '/gmail_callbacks/uptake', to: 'gmail_callbacks#uptake'
   get '/gmail_callbacks/callback', to: 'gmail_callbacks#callback'
   get '/gmail_callbacks/redirect', to: 'gmail_callbacks#redirect'
+
   get '/learn/new', to: 'learn#new'
+  
   post '/messages/safe_to_danger', to: 'messages#safe_to_danger'
   post '/messages/danger_to_safe', to: 'messages#danger_to_safe'
 end
