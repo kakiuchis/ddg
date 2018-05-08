86d6ea9c415ec2585b1c21ff50b6c7c352ae4ddc
Kakiuchi Shusuke <kakiuchis@gmail.com>
2018-05-09 00:25:57 +0900

    clean messages files

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,65 +1,26 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :set_message, only: [:edit, :update, :destroy, :safe_to_danger, :danger_to_safe]
   respond_to :js
 
-  # GET /messages
-  # GET /messages.json
   def index
     @messages = Message.user_choice_top_newer(current_user).all
   end
 
-  # GET /messages/1
-  # GET /messages/1.json
-  def show
-  end
-
-  # GET /messages/new
-  def new
-    @message = Message.new
-  end
-
-  # GET /messages/1/edit
   def edit
   end
 
-  # POST /messages
-  # POST /messages.json
-  def create
-    @message = Message.new(message_params)
-
-    respond_to do |format|
-      if @message.save
-        format.html { redirect_to @message, notice: 'Message was successfully created.' }
-        format.json { render :show, status: :created, location: @message }
-      else
-        format.html { render :new }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /messages/1
-  # PATCH/PUT /messages/1.json
   def update
-    respond_to do |format|
-      if @message.update(message_params)
-        format.html { redirect_to @message, notice: 'Message was successfully updated.' }
-        format.json { render :show, status: :ok, location: @message }
-      else
-        format.html { render :edit }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
+    if @message.update(message_params)
+      redirect_to messages_path, notice: 'Message was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /messages/1
-  # DELETE /messages/1.json
   def destroy
     @message.destroy
-    respond_to do |format|
-      format.html { redirect_to messages_url, notice: 'Message was successfully destroyed.' }
-      format.json { head :no_content }
+    redirect_to messages_path, notice: 'Message was successfully destroyed.'
     end
   end
 
@@ -69,26 +30,21 @@ class MessagesController < ApplicationController
   end
 
   def safe_to_danger
-    @message = Message.find(params[:id])
     @message.update(label: "danger")
     respond_with @message
   end
 
   def danger_to_safe
-    @message = Message.find(params[:id])
     @message.update(label: "safe")
     respond_with @message
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_message
       @message = Message.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def message_params
-      params.require(:message).permit(:message_id, :title, :body, :body_en, :label, :user_id)
-      # params.require(:message).permit(:label)
+      params.require(:message).permit(:id, :body_en)
     end
 end
--- app/views/messages/_message.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! message, :id, :message_id, :title, :body, :body_en, :label, :user_id, :created_at, :updated_at
-json.url message_url(message, format: :json)
