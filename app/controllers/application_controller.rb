03f8131721ef88c0c530b11af29256937f6aeff4
Kakiuchi Shusuke <kakiuchis@gmail.com>
2018-05-09 20:53:19 +0900

    deploy set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env.production?
   before_action :configure_permitted_parameters, if: :devise_controller?
   PERMISSIBLE_ATTRIBUTES = %i(boss_email slack_url)
 
