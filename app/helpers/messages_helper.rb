7ee73bd000a12a963ab336a8d1d024fd7c14cb74
Kakiuchi Shusuke <kakiuchis@gmail.com>
2018-05-12 17:21:42 +0900

    message index design

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,12 @@
 module MessagesHelper
+  def new_or_not(message)
+  	if Time.now - message.uptake_time < 3600
+  	  p "1時間以内に取り込まれたメール"
+  	else
+  	  p ""
+  	end
+  end
+  def time_format(time)
+  	time.in_time_zone('Tokyo').strftime("%Y/%m/%d %-H:%M")
+  end
 end
