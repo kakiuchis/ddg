30cf22b6f9acbb57ff91c972078efcc3006a3636
Kakiuchi Shusuke <kakiuchis@gmail.com>
2018-05-13 12:06:05 +0900

    add jon announce

--- app/controllers/detect_controller.rb
+++ app/controllers/detect_controller.rb
@@ -60,78 +60,13 @@ class DetectController < ApplicationController
     newer_than_hour = session[:newer_than_hour]
     query = "from:#{current_user.boss_email} to:#{current_user.email} newer_than:#{newer_than_hour}h"
     model_id = Learn.user_choice_one_newer(current_user).modelId
-    @analysises = []
-    i = 0
-    
-    ## get messages
+
     messages = get_messages(google_token, query)["messages"]
     if messages == nil
       redirect_to root_path, notice: "設定したBOSSのメールアドレス、指定した過去時間数ではメールがヒットしませんでした。" 
     else
-      messages.reverse.each do |message|
-        message_info = get_message_info(google_token, message["id"])
-
-        # ## get body
-        # if message_info["payload"]["parts"][0]["body"]["data"].present?
-        #   body = message_info["payload"]["parts"][0]["body"]["data"]
-        # elsif message_info["payload"]["parts"][1]["body"]["data"].present?
-        #   body = message_info["payload"]["parts"][1]["body"]["data"]
-        # else
-        #   body = message_info["payload"]["body"]["data"]
-        # end
-        # body = clean_body(body)
-        
-        ## get snippet
-        body = message_info["snippet"]
-        if body.present?
-          ## translate body
-          body_en = translate(body) 
-
-          # ## stop translate
-          # body_en = body
-
-          ## get analysis
-          if check_token(einstein_token) == "valid"
-            analysis = analysis(einstein_token, model_id, body_en)
-            @analysises.push(analysis)
-
-            ## if danger
-            if analysis["probabilities"][0]["label"] == "danger"
-              ## get danger_probability
-            	danger_probability = analysis["probabilities"][0]["probability"]
-
-              ## get receive_time and subject
-              message_info["payload"]["headers"].count.times do |i|
-                if message_info["payload"]["headers"][i]["name"] == "Date"
-                  @date = message_info["payload"]["headers"][i]["value"]
-                end
-                if message_info["payload"]["headers"][i]["name"] == "Subject"
-                  @subject = message_info["payload"]["headers"][i]["value"]
-                end
-              end
-
-              ## get url
-              url = "https://mail.google.com/mail/u/0/#all/#{message["id"]}"
-              
-              ## announce slack
-              text = "★★★危険度#{BigDecimal((danger_probability*100).to_s).ceil(1).to_f}%★★★\n【日時】#{@date}\n【件名】#{@subject}\n【本文】#{body}\n#{url}"
-              slack_annnounce(text, current_user.slack_url)
-              
-              ## count danger
-              i = i + 1
-            end
-          end
-        end
-      end
-      if i == 0
-        text = "過去#{newer_than_hour}時間で#{messages.count}件のBOSSからのメールがありましたが、危険なメールはなかったよ！セーフ！\n以下証拠です\n#{@analysises}"
-        slack_annnounce(text, current_user.slack_url)
-      end
-      if check_token(einstein_token) == "invalid"
-        redirect_to root_path, notice: "Einstain Tokenの有効期限で正しく検出できていない可能性があります。再度発行し、検出しなおしましょう。"
-      else
-        redirect_to root_path, notice: "Slackへの通知が完了しました。"
-      end
+      AnnounceJob.perform_later(messages, google_token, einstein_token, model_id, current_user, newer_than_hour)
+      redirect_to root_path, notice: "Slackへの通知処理を行っています。"
     end
   end
 end
